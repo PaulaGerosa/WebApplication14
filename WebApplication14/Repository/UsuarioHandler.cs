2468d3a293080843388db05dc65ad765153fc603
PaulaGerosa <paulagerosa1604@gmail.com>
2023-07-12 23:38:40 -0300

    Usuario

--- WebApplication14/Repository/UsuarioHandler.cs
+++ WebApplication14/Repository/UsuarioHandler.cs
@@ -1,16 +1,171 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
 using WebApplication14.Models;
+using WebApplication14.Repository;
 
 namespace WebApplication14.Repository
-{
+
+{ 
     public class UsuarioHandler
     {
         public const string connectionString = "Server=<LAPTOP-VU14H4RS\\SQLEXPRESS01> ; Database=<SistemaGestion>;Trusted_Connection=True;";
+
+        //Inicio de sesión 
+        public static Usuario InicioDeSesion(string nombreUsuario, string contraseña)
+        {
+            Usuario usuario = new Usuario();
+
+            
+            if (String.IsNullOrEmpty(nombreUsuario) || String.IsNullOrEmpty(contraseña)) 
+        {
+                return usuario; 
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("SELECT * FROM [SistemaGestion].[dbo].[Usuario] WHERE (NombreUsuario = @nombreUsuario AND Contraseña = @contraseña)", sqlConnection))
+                {
+                    var sqlParameter1 = new SqlParameter();
+                    sqlParameter1.ParameterName = "nombreUsuario";
+                    sqlParameter1.SqlDbType = SqlDbType.VarChar;
+                    sqlParameter1.Value = nombreUsuario;
+                    sqlCommand.Parameters.Add(sqlParameter1);
+
+                    var sqlParameter2 = new SqlParameter();
+                    sqlParameter2.ParameterName = "contraseña";
+                    sqlParameter2.SqlDbType = SqlDbType.VarChar;
+                    sqlParameter2.Value = contraseña;
+                    sqlCommand.Parameters.Add(sqlParameter2);
+
+                    sqlConnection.Open();
+
+                    using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
+                    {
+                        if (dataReader.HasRows & dataReader.Read())
+                        {
+                            usuario.Id = Convert.ToInt32(dataReader["Id"]);
+                            usuario.Nombre = dataReader["Nombre"].ToString();
+                            usuario.Apellido = dataReader["Apellido"].ToString();
+                            usuario.NombreUsuario = dataReader["NombreUsuario"].ToString();
+                            usuario.Contrasena = dataReader["Contraseña"].ToString();
+                            usuario.Mail = dataReader["Mail"].ToString();
+                        }
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            return usuario;
+        }
+
+        //Traer Usuario
+
+        private static Usuario TraerUsuario_conNombreUsuario(string nombreUsuario)
+        {
+            Usuario usuario = null;
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM [SistemaGestion].[dbo].[Usuario] WHERE NombreUsuario = @nombreUsuario";
+
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@nombreUsuario", nombreUsuario);
+
+                    sqlConnection.Open();
+
+                    using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
+                    {
+                        if (dataReader.Read())
+                        {
+                            usuario = new Usuario
+                            {
+                                Id = Convert.ToInt32(dataReader["Id"]),
+                                Nombre = dataReader["Nombre"].ToString(),
+                                Apellido = dataReader["Apellido"].ToString(),
+                                NombreUsuario = dataReader["NombreUsuario"].ToString(),
+                                Contrasena = dataReader["Contraseña"].ToString(),
+                                Mail = dataReader["Mail"].ToString()
+                            };
+                        }
+                    }
+
+                    sqlConnection.Close();
+                }
+            }
+
+            return usuario;
+        }
+
+        //Crear Usuario
+        public static bool CrearUsuario(Usuario usuario)
+        {
+            bool resultado = false;
+            int rowsAffected = 0;
+
+            
+            if (String.IsNullOrEmpty(usuario.Nombre) ||
+                String.IsNullOrEmpty(usuario.Apellido) ||
+                String.IsNullOrEmpty(usuario.NombreUsuario) ||
+                String.IsNullOrEmpty(usuario.Contrasena) ||
+                String.IsNullOrEmpty(usuario.Mail))
+            {
+                return false; 
+            }
+
+
+
+            // Verifico si el nombreUsuario ya existe
+            Usuario usuarioEnBD = new Usuario();
+            usuarioEnBD = TraerUsuario_conNombreUsuario(usuario.NombreUsuario); 
+            if (usuarioEnBD.Id != 0) 
+            {
+                return resultado; 
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                string queryInsert = "INSERT INTO [SistemaGestion].[dbo].[Usuario] (Nombre, Apellido, NombreUsuario, Contraseña, Mail) " + // Query que me permite crear un Usuario.
+                                        "VALUES (@nombre, @apellido, @nombreUsuario, @contraseña, @mail) ";
+
+                var parameterNombre = new SqlParameter("nombre", SqlDbType.VarChar);
+                parameterNombre.Value = usuario.Nombre;
+
+                var parameterApellido = new SqlParameter("apellido", SqlDbType.VarChar);
+                parameterApellido.Value = usuario.Apellido;
+
+                var parameterNombreUsuario = new SqlParameter("nombreUsuario", SqlDbType.VarChar);
+                parameterNombreUsuario.Value = usuario.NombreUsuario;
+
+                var parameterContraseña = new SqlParameter("contraseña", SqlDbType.VarChar);
+                parameterContraseña.Value = usuario.Contrasena;
+
+                var parameterMail = new SqlParameter("mail", SqlDbType.VarChar);
+                parameterMail.Value = usuario.Mail;
+
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand(queryInsert, sqlConnection))
+                {
+                    sqlCommand.Parameters.Add(parameterNombre);
+                    sqlCommand.Parameters.Add(parameterApellido);
+                    sqlCommand.Parameters.Add(parameterNombreUsuario);
+                    sqlCommand.Parameters.Add(parameterContraseña);
+                    sqlCommand.Parameters.Add(parameterMail);
+                    rowsAffected = sqlCommand.ExecuteNonQuery();
+                }
+                sqlConnection.Close();
+            }
+            if (rowsAffected == 1)
+            {
+                resultado = true;
+            }
+            return resultado;
+        }
+       
+
+
+        //Modificar Usuario
         public static bool ModificarUsuario(Usuario usuario)
         {
             bool resultado = false;
@@ -25,7 +180,7 @@ namespace WebApplication14.Repository
             if (String.IsNullOrEmpty(usuario.Nombre) ||
                 String.IsNullOrEmpty(usuario.Apellido) ||
                 String.IsNullOrEmpty(usuario.NombreUsuario) ||
-                String.IsNullOrEmpty(usuario.Contraseña) ||
+                String.IsNullOrEmpty(usuario.Contrasena) ||
                 String.IsNullOrEmpty(usuario.Mail))
             {
                 return false; 
@@ -51,7 +206,7 @@ namespace WebApplication14.Repository
                 parameterNombreUsuario.Value = usuario.NombreUsuario;
 
                 var parameterContraseña = new SqlParameter("contraseña", SqlDbType.VarChar);
-                parameterContraseña.Value = usuario.Contraseña;
+                parameterContraseña.Value = usuario.Contrasena;
 
                 var parameterMail = new SqlParameter("mail", SqlDbType.VarChar);
                 parameterMail.Value = usuario.Mail;
@@ -79,6 +234,42 @@ namespace WebApplication14.Repository
             }
             return resultado;
 
+        }
+
+        //Eliminar Usuario
+        public static bool EliminarUsuario(int id)
+        {
+            bool resultado = false;
+            int rowsAffected = 0;
+
+            
+            if (id <= 0) 
+            {
+                return false;
+            }
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                string queryDelete = "DELETE FROM [SistemaGestion].[dbo].[Usuario] " + 
+                                        "WHERE Id = @id";
+
+                var parameterId = new SqlParameter("id", SqlDbType.BigInt);
+                parameterId.Value = id;
+
+                sqlConnection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand(queryDelete, sqlConnection))
+                {
+                    sqlCommand.Parameters.Add(parameterId);
+                    rowsAffected = sqlCommand.ExecuteNonQuery();
+                }
+                sqlConnection.Close();
+            }
+            if (rowsAffected == 1)
+            {
+                resultado = true;
+            }
+            return resultado;
         }
     }
 }
